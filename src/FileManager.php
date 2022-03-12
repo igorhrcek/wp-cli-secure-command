ce862c444ab11c71247c83b3a2ad67db7e92bb8d
Igor Hrcek <igor.hrcek@mint.rs>
2022-03-12 18:53:07 +0100

    feat: Added support for writing into files

--- src/FileManager.php
+++ src/FileManager.php
@@ -2,17 +2,81 @@
 
 namespace WpCliFileManager;
 
-use WpCliFileManager\Exceptions\FileDoesNotExist;
-use function PHPUnit\Framework\throwException;
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
 
 class FileManager {
     /**
-     * @var string Full path of managed file
+     * Defines the end of each marked block
+     * # END WP CLI SECURE
+     */
+    public const MARKER_GLOBAL_END = '# END';
+
+    /**
+     * Defines the start of each marked block
+     * # BEGIN WP CLI SECURE
+     */
+    public const MARKER_GLOBAL_START = '# BEGIN';
+
+    /**
+     * Used to mark a start of WP CLI Secure block
+     */
+    public const MARKER_WP_CLI_SECURE = 'WP CLI SECURE';
+
+    /**
+     * This sting is used as a start of every rule block
+     * #### marker RULE NAME start ####
+     */
+    public const MARKER_RULE = '#### marker';
+
+    /**
+     * This string is used to mark the end of the starting line of every rule block
+     * #### marker RULE NAME start ####
+     */
+    public const MARKER_RULE_START = 'start ####';
+
+    /**
+     * This string is used to mark the end of the ending line of every rule block
+     * #### marker RULE NAME end ####
+     */
+    public const MARKER_RULE_END = 'end ####';
+
+    /**
+     * It is just a space
+     */
+    public const SPACE_DELIMITER = ' ';
+
+    /**
+     * @var string Full path of managed file, for example /home/site/www/nginx.conf
      */
     private string $path;
 
+    /**
+     * Extension used for backup files
+     */
+    private const BACKUP_EXTENSION = '.bkp';
+
+    /**
+     * @var array File content converted to an array
+     */
+    private array $file;
+
     public function __construct(string $path) {
         $this->path = $path;
+        $this->file = $this->setFileContent();
+    }
+
+    /**
+     * Set value of file
+     *
+     * @return array
+     */
+    private function setFileContent() : array {
+        if(!$this->fileExist()) {
+            return [];
+        }
+
+        return $this->read();
     }
 
     /**
@@ -54,7 +118,7 @@ class FileManager {
 
         unset($file);
 
-        return $this->removeZeroSpace($result);
+        return self::removeZeroSpace($result);
     }
 
     /**
@@ -103,5 +167,286 @@ class FileManager {
         }
 
         return str_replace(["\xe2\x80\x8b", "\xe2\x80\x8c", "\xe2\x80\x8d"], '', $content);
+    }
+
+    /**
+     * Extracts rules between two rule block markers
+     *
+     * @param string $marker
+     *
+     * @return array
+     */
+    public function extractRuleBlock(string $marker) : array {
+        $result = [];
+
+        $found = false;
+        foreach($this->file as $line) {
+            if(strpos($line, $marker . self::SPACE_DELIMITER . self::MARKER_RULE_END) !== false) {
+                $found = false;
+            }
+
+            if($found) {
+                $result[] = $line;
+            }
+
+            if(strpos($line, $marker . self::SPACE_DELIMITER . self::MARKER_RULE_START) !== false) {
+                $found = true;
+            }
+        }
+
+        return array_map('trim', $result);
+    }
+
+    /**
+     * Returns location of item inside array
+     *
+     * @param string $needle
+     *
+     * @return int|bool
+     */
+    private function findInFile(string $needle) : int|bool {
+        return array_search($needle, $this->file);
+    }
+
+    /**
+     * Extracts whole WP CLI SECURE block if it exist
+     *
+     * @return array|bool
+     */
+    public function extractSecureBlock(): bool|array {
+        $start = $this->findInFile(self::MARKER_GLOBAL_START . self::SPACE_DELIMITER . self::MARKER_WP_CLI_SECURE);
+        $end = $this->findInFile(self::MARKER_GLOBAL_END . self::SPACE_DELIMITER . self::MARKER_WP_CLI_SECURE);
+
+        if($start === false || $end === false) {
+            return false;
+        }
+
+        $result = array_slice($this->file, $start + 1, $end - $start - 1);
+
+        if(!$result) {
+            return false;
+        }
+
+        if(count($result) === 1) {
+            return trim($result[0]);
+        }
+
+        return array_filter($result);
+    }
+
+    /**
+     * Used to check if there is a single rule block in file
+     *
+     * @param string $marker
+     *
+     * @return bool
+     */
+    public function hasRuleBlock(string $marker) : bool {
+        return count($this->extractRuleBlock($marker)) > 0;
+    }
+
+    /**
+     * Checks if there is a global WP CLI SECURE present in the file
+     *
+     * @return bool
+     */
+    public function hasSecureBlock() : bool {
+        return is_array($this->extractSecureBlock()) && count($this->extractSecureBlock()) > 0;
+    }
+
+    /**
+     * Creates a file on a given path if it does not exist
+     *
+     * @param int $permissions
+     *
+     * @return bool
+     */
+    public function createFile(int $permissions = 0666) : bool {
+        if(!$this->fileExist()) {
+            $path = explode("/", $this->path);
+            $fileName = $path[count($path) - 1];
+            $basePath = implode('/', array_slice($path, 0, count($path) - 1));
+            $pathInfo = pathinfo($this->path);
+
+            //Create a full directory path if it does not exist
+            if(!file_exists($pathInfo['dirname'])) {
+                mkdir($pathInfo['dirname'], 0755, true);
+            }
+
+            //Create an empty file with given permissions
+            touch($this->path);
+            chmod($this->path, $permissions);
+
+            return true;
+        }
+
+        return true;
+    }
+
+    /**
+     * Wraps rules blocks with global or rule markers
+     *
+     * @param array  $content
+     * @param string $with
+     * @param string $marker
+     *
+     * @return array
+     */
+    public function wrap(array $content, string $with = 'block', string $marker = '') : array {
+        if($with === 'block') {
+            array_unshift($content, self::MARKER_RULE . self::SPACE_DELIMITER . $marker . self::SPACE_DELIMITER . self::MARKER_RULE_START);
+            array_push($content, self::MARKER_RULE . self::SPACE_DELIMITER . $marker . self::SPACE_DELIMITER . self::MARKER_RULE_END);
+        } else if($with === 'global') {
+            array_unshift($content, self::MARKER_GLOBAL_START . self::SPACE_DELIMITER . self::MARKER_WP_CLI_SECURE);
+            array_push($content, self::MARKER_GLOBAL_END . self::SPACE_DELIMITER . self::MARKER_WP_CLI_SECURE);
+        }
+
+        return $content;
+    }
+
+    /**
+     * Creates a backup of file before any changes are made to it
+     *
+     * @return bool
+     */
+    private function backup() : bool
+    {
+        if(file_exists($this->path . self::BACKUP_EXTENSION)) {
+            unlink($this->path . self::BACKUP_EXTENSION);
+        }
+
+        return copy($this->path, $this->path . self::BACKUP_EXTENSION);
+    }
+
+    /**
+     * Adds a new rule block
+     *
+     * @param array  $content
+     * @param string $marker
+     *
+     * @return bool
+     */
+    public function add(array $content, string $marker = '') {
+        //If the rule block already exist, there is no reason to add it again
+        if($this->hasRuleBlock($marker)) {
+            new Error(Error::RULE_ALREADY_EXIST);
+        }
+
+        if(!$this->isWritable()) {
+            new Error(Error::FILE_IS_NOT_WRITABLE);
+        }
+
+        //Check if file exist?
+        if(!$this->fileExist()) {
+            $this->createFile();
+        }
+
+        //Wrap the rule block with markers
+        $content = $this->wrap($content, 'block', $marker);
+
+        if(!$this->hasSecureBlock()) {
+            $content = $this->wrap($content, 'global');
+
+            //File does not contain any of our SECURE rules, so we need to prepend our content at the beginning of the file
+            array_unshift($this->file, $content);
+        } else {
+            //There is already the SECURE block in the file, so we need to append new rule block at the end of global block
+            $end = $this->findInFile(self::MARKER_GLOBAL_END . self::SPACE_DELIMITER . self::MARKER_WP_CLI_SECURE);
+
+            array_splice($this->file, $end, 0, $content);
+        }
+
+        return $this->write();
+    }
+
+    /**
+     * Removes all rules and global SECURE block from file
+     *
+     * @return bool
+     */
+    public function clear() : bool {
+        if(!$this->hasSecureBlock()) {
+            return true;
+        }
+
+        $start = $this->findInFile(self::MARKER_GLOBAL_START . self::SPACE_DELIMITER . self::MARKER_WP_CLI_SECURE);
+        $end = $this->findInFile(self::MARKER_GLOBAL_END . self::SPACE_DELIMITER . self::MARKER_WP_CLI_SECURE);
+
+        array_splice($this->file, $start, $end + 1 - $start);
+
+        return $this->write();
+    }
+
+    /**
+     * Removes one rule block
+     *
+     * @param string $marker
+     *
+     * @return bool
+     */
+    public function remove(string $marker) : bool {
+        if(!$this->hasRuleBlock($marker)) {
+            return true;
+        }
+
+        $start = $this->findInFile(self::MARKER_RULE . self::SPACE_DELIMITER . $marker . self::SPACE_DELIMITER . self::MARKER_RULE_START);
+        $end = $this->findInFile(self::MARKER_RULE . self::SPACE_DELIMITER . $marker . self::SPACE_DELIMITER . self::MARKER_RULE_END);
+
+        array_splice($this->file, $start, $end + 1 - $start);
+
+        return $this->write();
+    }
+
+    /**
+     * Writes changes to a file
+     *
+     * @return bool
+     */
+    public function write(): bool {
+        //Flatten the array (<= PHP 8.0 compatible)
+        $this->file = $this->flattenArray($this->file);
+
+        //Make a backup of a file
+        $this->backup();
+
+        $fp = fopen($this->path, 'w');
+        //Attempt to get a file lock, if FS has support
+        flock($fp, LOCK_EX);
+
+        //Write the content
+        $content = implode(PHP_EOL, $this->file);
+        fseek($fp, 0);
+        $write = fwrite($fp, $content);
+
+        if($write) {
+            ftruncate($fp, ftell($fp));
+        }
+        fflush($fp);
+        flock($fp, LOCK_UN);
+        fclose($fp);
+
+        return (bool) $write;
+    }
+
+    /**
+     * Flattens the array structure, for example:
+     * [1, 2, 3, [5, 6]] => [1, 2, 3, 5, 6]
+     *
+     * @param array $array
+     * @param int   $depth
+     *
+     * @return array
+     */
+    private function flattenArray(array $array, int $depth = 1) : array {
+        $result = [];
+        foreach ($array as $key => $value) {
+            if (is_array($value) && $depth) {
+                $result = array_merge($result, $this->flattenArray($value, $depth - 1));
+            } else {
+                $result = array_merge($result, [$key => $value]);
+            }
+        }
+
+        return $result;
     }
 }
