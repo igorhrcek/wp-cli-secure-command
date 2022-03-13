7ff223f831f0b23dfffd0ac66fcdec9804944e3f
Igor Hrcek <igor.hrcek@mint.rs>
2022-03-13 18:53:01 +0100

    feat: Cleaned up main file

--- secure-command.php
+++ secure-command.php
@@ -10,17 +10,21 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
-\WP_CLI::add_command('secure', Blah::class);
+\WP_CLI::add_command('secure', SecureCommand::class);
 
-//function registerCommand(CommandInterface $command) {
-//    if (!defined('WP_CLI') || !WP_CLI || !class_exists('\WP_CLI')) {
-//        return;
-//    }
-//
-//    \WP_CLI::add_command($command->getName(), $command, [
-//        'shortdesc' => $command->getDescription(),
-//        'synopsis'  => $command->getSynopsis()
-//    ]);
-//}
-//
-//registerCommand(new SecureCommand());
+//wp secure disable-directory-browsing
+//wp secure block-php-execution-in-uploads
+//wp secure block-php-execution-in-themes
+//wp secure block-php-execution-in-plugins
+//wp secure block-php-execution-in-wp-includes
+//wp secure block-access-to-htaccess
+//wp secure block-access-to-sensitive-files
+//wp secure block-access-to-sensitive-directories
+//wp secure block-author-scanning
+//wp secure block-access-to-xml-rpc
+
+//wp secure add --rules=disable-directory-browsing,block-php-execution-in-uploads --output --path
+//wp secure remove --rules=disable-directory-browsing,block-php-execution-in-uploads
+//wp secure flush
+//wp secure block-access-to-htaccess --revert
+//wp secure block-access-to-htaccess --path=/path/to/htaccess --output
