b8b17edc4c3fdfda79ff42b4d27ec0b465a02b6d
Igor Hrcek <igor.hrcek@mint.rs>
2022-03-14 19:23:03 +0100

    feat: Added bootstrapping for WP_CLI testing#

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -17,6 +17,17 @@ class BaseTestCase extends TestCase {
         $this->root = vfsStream::setup('assets', 0777);
         $this->root->chown(vfsStream::getCurrentUser());
         $this->root->chgrp(vfsStream::getCurrentGroup());
+
+        //Load dependencies for the WP_CLI tests
+        if (!defined('WP_CLI_ROOT')) {
+            define('WP_CLI_ROOT', dirname(__DIR__) . '/vendor/wp-cli/wp-cli');
+        }
+        require_once WP_CLI_ROOT . '/php/utils.php';
+        require_once WP_CLI_ROOT . '/php/dispatcher.php';
+        require_once WP_CLI_ROOT . '/php/class-wp-cli.php';
+        require_once WP_CLI_ROOT . '/php/class-wp-cli-command.php';
+
+        \WP_CLI\Utils\load_dependencies();
     }
 
     /**
