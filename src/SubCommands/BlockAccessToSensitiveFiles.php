536d03eabd3e45b455f0ec4d6cdab904ae2e18ca
Igor Hrcek <igor.hrcek@mint.rs>
2022-03-20 22:31:57 +0100

    fix: Fixed an issue that caused incorrect rules to be applied for certain files

--- src/SubCommands/BlockAccessToSensitiveFiles.php
+++ src/SubCommands/BlockAccessToSensitiveFiles.php
@@ -7,33 +7,4 @@ class BlockAccessToSensitiveFiles extends SubCommand {
     public string $ruleName = 'BLOCK ACCESS TO SENSITIVE FILES';
     public string $successMessage = 'Block Access to Sensitive Files rule has been deployed.';
     public string $removalMessage= 'Block Access to Sensitive Files rule has been removed.';
-
-    /**
-     * @var string A list of files that should be protected by default
-     */
-    private string $sensitiveFiles = 'readme.html, readme.txt, wp-config.php, wp-admin/install.php';
-
-    /**
-     * @return array
-     */
-    public function getTemplateVars(): array {
-        $files = $this->commandArguments['files'] ?? $this->sensitiveFiles;
-
-        if (!empty($files)) {
-            $files = explode(',', $files);
-            $files = array_map('trim', $files);
-            $files_array = [];
-
-            foreach ($files as $key => $value) {
-                $file = (isset($this->commandArguments['server']) && $this->commandArguments['server'] === 'nginx') ?
-                    preg_quote($value) : $value;
-
-                $files_array[] = ['file' => $file];
-            }
-            
-            return $files_array;
-        }
-
-        return [];
-    }
 }
