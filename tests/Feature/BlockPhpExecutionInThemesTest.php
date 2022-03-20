f892615fa081a9a7bbc2b5d603622c9b20b93770
Igor Hrcek <igor.hrcek@mint.rs>
2022-03-20 22:37:08 +0100

    fix: Fixed incorrect path for calling nginx reload command

--- tests/Feature/BlockPhpExecutionInThemesTest.php
+++ tests/Feature/BlockPhpExecutionInThemesTest.php
@@ -15,7 +15,7 @@ class BlockPhpExecutionInThemesTest extends BaseTestCase {
         $command = new BlockPhpExecutionInThemes($this->apacheAssocArgs);
         $command->output();
 
-        exec('cd ' . dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . $_ENV['WORDPRESS_NGINX_PATH'] . '&& ddev exec nginx -s reload');
+        exec('cd ' . getcwd() . DIRECTORY_SEPARATOR . $_ENV['WORDPRESS_NGINX_PATH'] . '&& ddev exec nginx -s reload');
     }
 
     public function testItWillReturnHttp403OnNginxWhenAccessingPhpFilesInThemesDirectory() : void {
