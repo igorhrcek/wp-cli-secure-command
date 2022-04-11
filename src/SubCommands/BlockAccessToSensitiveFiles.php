eb2348c57ad892a519b8280b9e4c124e2d1f908d
Igor Hrcek <igor.hrcek@mint.rs>
2022-04-11 09:22:35 +0200

    fix: Fixed an issue that caused duplication of rules

--- src/SubCommands/BlockAccessToSensitiveFiles.php
+++ src/SubCommands/BlockAccessToSensitiveFiles.php
@@ -8,35 +8,4 @@ class BlockAccessToSensitiveFiles extends SubCommand
     public string $ruleName = 'BLOCK ACCESS TO SENSITIVE FILES';
     public string $successMessage = 'Block Access to Sensitive Files rule has been deployed.';
     public string $removalMessage = 'Block Access to Sensitive Files rule has been removed.';
-
-    /**
-     * @var string List of files that we are protecting by default
-     */
-    private string $protectedFiles = 'readme.html,readme.txt,wp-config.php,nginx.conf,/wp-admin/install.php,/wp-admin/upgrade.php';
-
-    public function getTemplateVars(): array
-    {
-        $files = $this->commandArguments['files'] ?? $this->protectedFiles;
-        if (! empty($files)) {
-            $files = explode(',', $files);
-            $files = array_map('trim', $files);
-            $files_array = [];
-
-            foreach ($files as $key => $value) {
-                if (preg_match('/.+\/.+/', $value)) {
-                    $file_with_directory = $this->setRuleContent(false, 'block_access_to_sensitive_files_with_directories');
-                    if (isset($this->commandArguments['server']) && $this->commandArguments['server'] === 'nginx') {
-                        $file = $value;
-                    } else {
-                        $file = preg_quote(ltrim($value, '/'));
-                    }
-                    $files_array[] = [ $file => $file_with_directory ];
-                } else {
-                    $files_array[] = [ 'file' => isset($this->commandArguments['server']) && $this->commandArguments['server'] === 'nginx' ? preg_quote($value) : $value ];
-                }
-            }
-            return $files_array;
-        }
-        return [];
-    }
 }
